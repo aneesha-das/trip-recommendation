b7d0bc0a2d1d562878dd3210d5715c406ac47abf
Amit Banerjee <parkouramit@gmail.com>
2020-04-22 23:38:53 +0530

    User list extracted. Working on user place mapping

---
+++ UserPlacesMapping.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 23:08:10 2020
+
+@author: Amit
+"""
+
+import pandas as pd
+import numpy as np
+data=pd.read_csv("place_recommendation_place_filtered.csv")
+data_copy=data.copy(deep=True).to_numpy()
+user_list=[];
+place_data=data_copy[:,5:25]
+user_list=data_copy[:,1]
+places=pd.read_csv("places.csv")
+data_copy=places.copy(deep=True).to_numpy()
+users=pd.read_csv("users.csv")
+users_data_copy=users.copy(deep=True).to_numpy()
+
+def getPlaceId(place_name):
+    place_id=0
+    for place in data_copy:
+        if(place[1]==place_name):
+            place_id=place[0]
+            break
+    return place_id
+
+def getUserId(user_name):
+    user_id=0
+    for user in users_data_copy:
+        if(user[1]==user_name):
+            user_id=user[0]
+            break
+    return user_id
+print(user_list[15])
+print(getUserId(user_list[15]))
---
+++ filterOutUniqueUsers.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 19:43:32 2020
+
+@author: Amit
+"""
+
+import pandas as pd
+import numpy as np
+data=pd.read_csv("place_recommendation_place_filtered.csv")
+data_copy=data.copy(deep=True).to_numpy()
+user_list=[];
+user_data=data_copy[:,1:5]
+for user in user_data:
+    user_name=user[0]
+    user_age=user[1]
+    user_gender=user[2]
+    user_state=user[3]
+    user_list.append([user_name,user_age,user_gender,user_state])
+ids=np.arange(start=1,stop=len(user_list)+1,step=1,dtype=int)
+user_list=np.insert(user_list,0,ids,axis=1)
+users=pd.DataFrame(user_list,columns=['Id','Name','Age','Gender','State'])
+users.to_csv('users.csv', index=False)
